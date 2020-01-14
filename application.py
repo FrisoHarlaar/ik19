2c7c6e9d7aee7f8f61b338dc94bb76856e427305
richardvdaardweg <richard.vandenaardweg@student.uva.nl>
2020-01-14 10:29:42 +0000

    created login.html and login route in application.py

--- application.py
+++ application.py
@@ -27,6 +27,41 @@ def after_request(response):
 def index():
     return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    if request.method == "GET":
+        return render_template("login.html")
+
+    # User reached route via POST (as by submitting a form via POST)
+    else:
+        # Assign form input to local dict
+        form = {"username": request.form.get("username"), "password": request.form.get("password")}
+
+        # Ensure form was fully filled out
+        for form_item in form.items():
+            if form_item[1] == '':
+                message = "must provide " + form_item[0]
+                return render_template("apology.html", message=message, code=400)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("apology.html", message="invalid username and/or password", code=400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
 
