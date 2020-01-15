2da1a6fee73b4e42bf850470fde79b039cf59b4e
Friso Harlaar <harlaar.friso@gmail.com>
2020-01-15 11:50:36 +0000

    added helpers.py and routes

---
+++ helpers.py
@@ -0,0 +1,16 @@
+import requests
+import urllib.parse
+import os
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+def login_required(f):
+
+    # make route require login
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
