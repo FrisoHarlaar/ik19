0b467c609d93c70e2a6dea660bed4618b64ea0ca
Friso Harlaar <friso.harlaar@student.uva.nl>
2021-01-26 14:52:22 +0000

    last changes before going live

--- application.py
+++ application.py
@@ -15,19 +15,11 @@ from tempfile import mkdtemp
 # Configure application
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SECRET_KEY"] = b'\xd3\xad\xd7\xb6\x16\x03t\x83M\xab4o)o\xb0\xeb'
 
 # Configure CS50 Library to use SQLite database
-db = SQL("postgres://uomwmqgrrsluox:e24e93a85d1bc3a77160440730810b49ee256e8552117a8623e12f67e818357c@ec2-54-228-170-125.eu-west-1.compute.amazonaws.com:5432/dfr7jp5obkns6k")
+db = SQL(os.env.getenv("DATABASE_URL", "sqlite:///brainbrawlers.db"))
 
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
